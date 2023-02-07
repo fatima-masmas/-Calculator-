7508cb561fb584cb7fe01b79a4bdb9f78c48e25d
fatima-masmas <87-f@gmx.de>
2023-02-07 06:10:59 +0100

    react-textfit install

--- calculator-app/src/components/Screen.js
+++ calculator-app/src/components/Screen.js
@@ -1,6 +1,9 @@
 import React from "react";
+import { useContext } from "react";
+import { CalcContext } from "../context/CalcContext";
 
 const Screen = () => {
+    const { calc} = useContext (CalcContext);
     return (
         <div className="screen">0321432</div>
     )
