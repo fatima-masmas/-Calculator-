10af610a09976f252a6344449813bfbca86ec82b
fatima-masmas <87-f@gmx.de>
2023-02-07 06:14:42 +0100

    Textfit anwenden

--- calculator-app/src/components/Screen.js
+++ calculator-app/src/components/Screen.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { useContext } from "react";
 import { CalcContext } from "../context/CalcContext";
+import { Textfit } from 'react-textfit';
 
 const Screen = () => {
     const { calc} = useContext (CalcContext);
     return (
-        <div className="screen">0321432</div>
+        <Textfit className="screen">0321432</Textfit>
     )
 }
 
